24cdd5aaf259b034fbe1ee7afff684bf50e8fb91
jbradford55 <122580688+jbradford55@users.noreply.github.com>
2024-02-16 14:49:45 -0800

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -14,24 +14,84 @@
  * 100Hz is 100 times per second
  */
 #define HZ 100
+static struct itimerval current_timer; 
+static struct itimerval previous_timer;
 
+static struct sigaction sig_action;
+static struct sigaction previous_sig_action;
+static sigset_t sig_set;
+static sigset_t previous_sig_set;
+
+
+static int preempt_enabled;
+
+void signal_handler(int input_signal){
+	if (input_signal == SIGVTALRM){
+		uthread_yield();
+	}
+}
 void preempt_disable(void)
 {
+	if (preempt_enabled == 1){
+		sigprocmask(SIG_BLOCK,&sig_set, NULL);
+	} else {
+		return;
+	}
 	/* TODO Phase 4 */
 }
 
 void preempt_enable(void)
 {
+	if (preempt_enabled==1){
+		sigprocmask(SIG_UNBLOCK,&sig_set, NULL );
+	} else {
+		return;
+	}
 	/* TODO Phase 4 */
 }
 
 void preempt_start(bool preempt)
 {
+	if (preempt){
+		preempt_enabled = 1;
+	} else {
+		return;
+	}
+
+	current_timer.it_interval.tv_usec = 10000;
+	current_timer.it_value.tv_usec = 10000;
+	setitimer(ITIMER_VIRTUAL, &current_timer, &previous_timer);
+
+
+
+
+
+	sigemptyset(&sig_set);
+	sigaddset(&sig_set, SIGVTALRM);
+
+
+	sig_action.sa_handler = signal_handler;
+	sigemptyset(&sig_action.sa_mask);
+	sig_action.sa_flags = 0;
+
+
+
+	sigaction(SIGVTALRM, &sig_action, &previous_sig_action);
+	sigprocmask(SIG_SETMASK, NULL, &previous_sig_set);
+
+	return;
 	/* TODO Phase 4 */
 }
 
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
+	if (preempt_enabled==1){
+		setitimer(SIGVTALRM, &previous_timer, NULL);
+		sigaction(SIGVTALRM, &previous_sig_action, NULL);
+		sigprocmask(SIG_SETMASK, &previous_sig_set, NULL);
+	} else {
+		return;
+	}
 }
 
